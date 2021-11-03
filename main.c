af9995663ab30d98edc79f4e7e29e22324532acc
Phoenix661T <thabangmaloka661@outlook.com>
2021-11-03 11:01:52 -0700

    main file

--- main.c
+++ main.c
@@ -1,178 +1,115 @@
 #include "shell.h"
-/**
- * main - program starts here
- * Return: 0 on success)
- */
-int main(void)
-{
-  sh_t data;
-int pl;
 
-_memset((void *)&data, 0, sizeof(data));
-signal(SIGINT, signal_handler);
+char **commands = NULL;
+char *line = NULL;
+char *shell_name = NULL;
+int status = 0;
+
+/**
+ * main - entry point, the main shell
+ * @argc: number of arguments passed to the program
+ * @argv: arguments passed to the program
+ *
+ * Return: 0 on success
+ */
+int main(int argc __attribute__((unused)), char **argv)
+{
+char **current_command = NULL;
+int i, type_command = 0;
+size_t n = 0;
+
+signal(SIGINT, ctrl_c_handler);
+shell_name = argv[0];
 while (1)
 {
-index_cmd(&data);
-if (read_line(&data) < 0)
+non_interactive();
+print("$ ", STDOUT_FILENO);
+if (getline(&line, &n, stdin) == -1)
 {
-if (isatty(STDIN_FILENO))
-PRINT("\n");
+free(line);
+exit(status);
+}
+remove_newline(line);
+remove_comment(line);
+commands = tokenizer(line, ";");
+for (i = 0; commands[i] != NULL; i++)
+{
+current_command = tokenizer(commands[i], " ");
+if (current_command[0] == NULL)
+{
+free(current_command);
 break;
 }
-if (split_line(&data) < 0)
-{
-free_data(&data);
-continue;
+type_command = parse_command(current_command[0]);
+initalizer(current_command, type_command);
+free(current_command);
 }
-pl = parse_line(&data);
-if (pl == 0)
-{
-free_data(&data);
-continue;
+free(commands);
 }
-if (pl < 0)
-{
-print_error(&data);
-continue;
-}
-if (process_cmd(&data) < 0)
-{
-print_error(&data);
-break;
-}
-free_data(&data);
-}
-free_data(&data);
-exit(EXIT_SUCCESS);
+free(line);
+return (status);
 }
 
 /**
- * read_line - read a line from the standard input
- * @data: a pointer to the struct of data
+ * initalizer - starts exectuting everything xD
+ * @current_command: try to check current token
+ * @type_command: parse token
  *
- * Return: (Success) a positive number
- * ------- (Fail) a negative number
+ * Return: void function
  */
-int read_line(sh_t *data)
-{
-char *csr_ptr, *end_ptr, c;
-size_t size = BUFSIZE, read_st, length, new_size;
 
-data->line = malloc(size * sizeof(char));
-if (data->line == NULL)
-return (FAIL);
-if (isatty(STDIN_FILENO))
-PRINT(PROMPT);
-for (csr_ptr = data->line, end_ptr = data->line + size;;)
+void initalizer(char **current_command, int type_command)
 {
-read_st = read(STDIN_FILENO, &c, 1);
-if (read_st == 0)
-return (FAIL);
-*csr_ptr++ = c;
-if (c == '\n')
-{
-*csr_ptr = '\0';
-return (SUCCESS);
-}
-if (csr_ptr + 2 >= end_ptr)
-{
-new_size = size * 2;
-length = csr_ptr - data->line;
-data->line = _realloc(data->line, size * sizeof(char),
-new_size * sizeof(char));
-if (data->line == NULL)
-return (FAIL);
-size = new_size;
-end_ptr = data->line + size;
-csr_ptr = data->line + length;
-}
-}
-}
+pid_t child;
 
-#define DELIMITER " \n\t\r\a\v"
-/**
- * split_line - splits line to tokens
- * @data: a pointer to the struct of data
- *
- * Return: (Success) a positive number
- * ------- (Fail) a negative number
- */
-int split_line(sh_t *data)
+if (type_command == EXTERNAL_COMMAND || type_command == PATH_COMMAND)
 {
-char *token;
-size_t size = TOKENSIZE, new_size, i = 0;
-
-if (_strcmp(data->line, "\n") == 0)
-return (FAIL);
-data->args = malloc(size * sizeof(char *));
-if (data->args == NULL)
-return (FAIL);
-token = strtok(data->line, DELIMITER);
-if (token == NULL)
-return (FAIL);
-while (token)
-{
-data->args[i++] =  token;
-if (i + 2 >= size)
-{
-new_size = size * 2;
-data->args = _realloc(data->args, size * sizeof(char *),
-new_size * sizeof(char *));
-if (data->args == NULL)
-return (FAIL);
-size = new_size;
-}
-token = strtok(NULL, DELIMITER);
-}
-data->args[i] = NULL;
-return (0);
-}
-#undef DELIMITER
-#define DELIMITER ":"
-/**
- * parse_line - parses arguments to valid command
- * @data: a pointer to the struct of data
- *
- * Return: (Success) a positive number
- * ------- (Fail) a negative number
- */
-int parse_line(sh_t *data)
-{
-if (is_path_form(data) > 0)
-return (SUCCESS);
-if (is_builtin(data) > 0)
-{
-if (handle_builtin(data) < 0)
-return (FAIL);
-return (NEUTRAL);
-}
-is_short_form(data);
-return (SUCCESS);
-}
-#undef DELIMITER
-/**
- * process_cmd - process command and execute process
- * @data: a pointer to the struct of data
- *
- * Return: (Success) a positive number
- * ------- (Fail) a negative number
- */
-int process_cmd(sh_t *data)
-{
-pid_t pid;
-int status;
-
-pid = fork();
-if (pid == 0)
-{
-signal(SIGINT, SIG_DFL);
-if (execve(data->cmd, data->args, environ) < 0)
-data->error_msg = _strdup("not found\n");
-return (FAIL);
-}
+child = fork();
+if (child == 0)
+execute_command(current_command, type_command);
 else
 {
-waitpid(pid, &status, WUNTRACED);
+waitpid(child, &status, 0);
+status >>= 8;
 }
-return (0);
+}
+else
+execute_command(current_command, type_command);
+}
+
+/**
+ * non_interactive - handles non_interactive mode
+ *
+ * Return: void
+ */
+void non_interactive(void)
+{
+char **current_command = NULL;
+int i, type_command = 0;
+size_t n = 0;
+
+if (!(isatty(STDIN_FILENO)))
+{
+while (getline(&line, &n, stdin) != -1)
+{
+remove_newline(line);
+remove_comment(line);
+commands = tokenizer(line, ";");
+for (i = 0; commands[i] != NULL; i++)
+{
+current_command = tokenizer(commands[i], " ");
+if (current_command[0] == NULL)
+{
+free(current_command);
+break;
+}
+type_command = parse_command(current_command[0]);
+initalizer(current_command, type_command);
+free(current_command);
+}
+free(commands);
+}
+free(line);
+exit(status);
+}
 }
