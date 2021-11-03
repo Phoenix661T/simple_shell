65b141500afc18f2956a3644802b9c9b420fb8e7
Phoenix661T <thabangmaloka661@outlook.com>
2021-11-03 13:43:02 -0700

    betty code

---
+++ betty.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ * Description: beautiful code that passes Betty checks
+ * Return: Always 0
+ */
+
+int main(void)
+{
+printf("Hello beautiful Betty, check me out!\n");
+return (0);
+}
